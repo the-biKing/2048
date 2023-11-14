d6004cb65d7af9462cdffca9161b5cbef371010d
zzzjkyyy <zzzjacky418@gmail.com>
2023-11-14 18:17:06 +0800

    new file:   2048_vs/.vs/2048_vs/FileContentIndex/0a2a91f5-2de5-4a57-8214-6bdd59c8199b.vsidx 	deleted:    2048_vs/.vs/2048_vs/FileContentIndex/7deedbb8-bc9d-46f4-96a9-6e57a3bded5f.vsidx modified:   2048_vs/.vs/2048_vs/v17/.suo modified:   2048_vs/.vs/2048_vs/v17/Browse.VC.db modified:   2048_vs/2048_vs/2048.c

--- 2048_vs/2048_vs/2048.c
+++ 2048_vs/2048_vs/2048.c
@@ -1,44 +1,50 @@
+<<<<<<< Updated upstream
 #include<stdio.h>
 #include<stdbool.h>
 #include"matrixManipulation.h"
 
 char checkState(int matrix[4][4]);
+=======
+#include <stdio.h>
+#include <stdbool.h>
+>>>>>>> Stashed changes
 
-int main(void) {
+int main(void)
+{
 	int board[4][4];
-	bool gameContinue=true;
+	bool gameContinue = true;
 	char playerInput;
-	for(int i=0;i<4;i++)
+	for (int i = 0; i < 4; i++)
 	{
-		for(int j=0;j<4;j++)
+		for (int j = 0; j < 4; j++)
 		{
-			board[i][j]=0;
+			board[i][j] = 0;
 		}
 	}
 	while (gameContinue)
 	{
-		for(int i=0;i<4;i++)
+		for (int i = 0; i < 4; i++)
 		{
-			for(int j=0;j<4;j++)
+			for (int j = 0; j < 4; j++)
 			{
-				printf("%d",board[i][j]);
+				printf("%d", board[i][j]);
 			}
 			printf("\n");
 		}
-		scanf("%c",&playerInput);
-		if(playerInput=='w')
+		scanf("%c", &playerInput);
+		if (playerInput == 'w')
 		{
 			moveUp(board[4][4]);
 		}
-		else if (playerInput=='a')
+		else if (playerInput == 'a')
 		{
 			moveLeft(board[4][4]);
 		}
-		else if(playerInput=='s')
+		else if (playerInput == 's')
 		{
 			moveDown(board[4][4]);
 		}
-		else if(playerInput=='d')
+		else if (playerInput == 'd')
 		{
 			moveRight(board[4][4]);
 		}
@@ -47,20 +53,23 @@ int main(void) {
 			printf("Invalid input");
 		}
 		addNumber(board[4][4]);
-		if(checkState!='c'){
-			gameContinue=false;
+		if (checkState != 'c')
+		{
+			gameContinue = false;
 		}
 	}
-	if(checkState=='w'){
+	if (checkState == 'w')
+	{
 		printf("win");
 	}
-	if(checkState=='l'){
+	if (checkState == 'l')
+	{
 		printf("lose");
 	}
 
 	return 0;
 }
 
-char checkState(int matrix[4][4]){
-
+char checkState(int matrix[4][4])
+{
 }
