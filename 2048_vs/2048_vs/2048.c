dc91648264fe4860225d95fef77c9afc4769d2cf
josh hsieh <joshhsieh1129@gmail.com>
2023-11-15 13:33:16 +0800

    addNumber function complete

--- 2048_vs/2048_vs/2048.c
+++ 2048_vs/2048_vs/2048.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
 #include "matrixManipulation.h"
 
 char checkState(int matrix[4][4]);
--- 2048_vs/2048_vs/matrixManipulation.c
+++ 2048_vs/2048_vs/matrixManipulation.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
 #include"matrixManipulation.h"
+#include<stdio.h>
+#include<time.h>
+#include<stdbool.h>
 
 void moveUp(int matrix[4][4]){
 
@@ -14,5 +16,34 @@ void moveDown(int matrix[4][4]){
 
 }
 void addNumber(int matrix[4][4]){
-    
+    srand(time(NULL));
+    bool keepFinding=true;
+    int countZero=0;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(matrix[i][j]==0){
+                countZero++;
+            }
+        }
+    }
+    if(countZero==0){
+        keepFinding=false;
+    }
+    while(keepFinding){
+        int i=rand()%4;
+        int j=rand()%4;
+        int twoOrfour=rand()%10;
+        if(matrix[i][j]==0){
+            if(twoOrfour<=7){
+                matrix[i][j]=2;
+                keepFinding=false;
+            }
+            else{
+                matrix[i][j]=4;
+                keepFinding=false;
+            }
+        }
+
+    }
+    return;
 }
